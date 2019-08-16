e1904a9ca226f3fb231fc1e70fb4895d29a539dc
shsandro <shsandro@github.com>
2019-08-16 16:35:38 -0300

    passado no teste fatorial

---
+++ sim/testsAssembly/factorial.asm
@@ -0,0 +1,12 @@
+addi $t0, $zero, 0x6
+addi $t1, $zero, 0x1
+addi $t2, $zero, 0x2
+addi $t3, $zero, 0x0
+beq $t0, $t1, end
+beq $t0, $zero, end
+loop:
+mul $t1, $t2, $t1
+addi $t2, $t2, 0x1
+addi $t3, $t3, 0x1
+bne $t3, $t0, loop
+end:
